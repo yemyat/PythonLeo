d51452209f5c5329ad74b4616fc1ed9932b58cbf
emoosx <emoosx@gmail.com>
2010-11-24 23:44:30 +0800

    Fixed URL

--- LeoPicRetriever.py
+++ LeoPicRetriever.py
@@ -1,24 +1,21 @@
-import PythonLeo
-# retrieves the photo of the student that you want to retrieve
-
+import urllib2
+import os
+# retrieves the photo of the Student or ( Staff ~ coming soon )
 def main():
-	url = 'http://myrp.sg/SASS-Student/Photo/'
-	myid = '91178'
-	mypassword = 'pyro1$cute'
+	url = 'http://www.rp.edu.sg/staffdirectory/ShowImage.aspx?id='
 	
 	student_id = raw_input("Student ID : ")
 	name = raw_input("Name : ")
 	name2 = name.upper().replace(' ', '%20')
 	url = url + student_id + '-' + name2 + '.jpg'
-	
-	login = PythonLeo.PythonLeo(myid, mypassword)
-	image = login.open_url(url).read()
+	image = urllib2.urlopen(url).read()
 	filename = name + '.jpg'
 	
 	f = open(filename, "wb")
 	f.write(image)
 	f.close()
 	print 'Done'
+	os.startfile(image)
 	
 if __name__ == '__main__':
 	main()
