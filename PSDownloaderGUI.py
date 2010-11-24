b532c885a6ba1c04a6183ca52b9828b706f32ad4
Ye Myat Min <yemyat@gmail.com>
2010-11-24 19:37:37 +0800

    Added threading and response messages

--- PSDownloaderGUI.py
+++ PSDownloaderGUI.py
@@ -2,13 +2,15 @@ import re
 import os
 import PythonLeo
 from Tkinter import *
-		
-def download(dummy=1):
-	URL_LIST = {"current_module":"http://leo.rp.edu.sg/workspace/studentModule.asp?", #to get project_id , group_id
+import threading
+
+URL_LIST = {"current_module":"http://leo.rp.edu.sg/workspace/studentModule.asp?", #to get project_id , group_id
 				"current_problem":"http://leo3.rp.edu.sg//projectweb/project_menu.asp?", #to get topic_id, year2,3=>leo3, year1=>leo1
 				"problem_download":"http://leo.rp.edu.sg/projectweb/projectupload/savefolderas.asp?folder=/databank/projectbank/"
-	};
+};
 
+def download():
+	response.set("Connecting")
 	leo = PythonLeo.PythonLeo(username_field.get(),password_field.get()) #e.g. 91224, 12345
 	project_id_list = leo.parse_id("projectid",leo.open_url(URL_LIST["current_module"]))
 	group_id_list = leo.parse_id("groupid",leo.open_url(URL_LIST["current_module"]))
@@ -19,10 +21,16 @@ def download(dummy=1):
 	get_download_url = leo.open_url(URL_LIST["problem_download"]+topic_id_list[-1])
 	
 	download_url = "http://leo.rp.edu.sg"+ re.search('HREF=\"(.+?zip)',get_download_url.read()).groups()[0]
+	response.set("Downloading")
 	os.chdir(os.path.expanduser("~/Desktop"))
 	zip_file = open("problem.zip","wb")
 	zip_file.write(	leo.open_url(download_url).read() )
 	zip_file.close()
+	
+	response.set("Done!")
+	
+def download_thread(dummy=1):	
+	threading.Thread(target=download).start()
 
 if __name__ == "__main__":
 	root = Tk()
@@ -42,9 +50,15 @@ if __name__ == "__main__":
 	
 	password_field =Entry(main_frame,show="*")
 	password_field.grid(column=1,row=1,columnspan=2)
-	password_field.bind("<Return>",download)
+	password_field.bind("<Return>",download_thread)
+	
+	response = StringVar()
+	response.set("")
+	
+	response_label = Label(main_frame, textvariable=response,fg="red",anchor=W,justify=LEFT)
+	response_label.grid(column=0,row=2)
 
-	dl_button = Button(main_frame,text="Download",command=download)
+	dl_button = Button(main_frame,text="Download",command=download_thread)
 	dl_button.grid(column=2,row=2)
 	
 	root.mainloop()
