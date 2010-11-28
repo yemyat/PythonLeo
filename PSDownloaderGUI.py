36aa57a3907b4eb7c3ac8c15a88ff93e7b60429d
Ye Myat Min <yemyat@gmail.com>
2010-11-28 22:20:02 +0800

    Added exceptions

--- PSDownloaderGUI.py
+++ PSDownloaderGUI.py
@@ -1,16 +1,12 @@
-import re
-import os
+import re, os, threading, zipfile, shutil
 import PythonLeo
 from Tkinter import *
-import threading
-import zipfile,shutil
+import tkMessageBox
 
 '''
 TODO
-1. Add error-handling
-2. If the last problem was assignment, download the one before.
+1. Verify error-handling
 '''
-
 
 URL_LIST = {"current_module":"http://leo.rp.edu.sg/workspace/studentModule.asp?site=3", #to get project_id , group_id
 				"current_problem":"http://leo3.rp.edu.sg//projectweb/project_menu.asp?", #to get topic_id, year2,3=>leo3, year1=>leo1
@@ -18,42 +14,46 @@ URL_LIST = {"current_module":"http://leo.rp.edu.sg/workspace/studentModule.asp?s
 };
 
 def download():
-	response.set("Connecting") 
+	response.set("Connecting")
 	leo = PythonLeo.PythonLeo(username_field.get(),password_field.get()) #e.g. 91224, 12345
-	project_id_list = leo.parse_id("projectid",leo.open_url(URL_LIST["current_module"]))
-	group_id_list = leo.parse_id("groupid",leo.open_url(URL_LIST["current_module"]))
-	topic_id_list = leo.parse_id("topicid",leo.open_url(URL_LIST["current_problem"]+
-										"projectid="+str(project_id_list[-1])+
-										"&groupid="+str(group_id_list[-1])))
-
-	get_download_url = leo.open_url(URL_LIST["problem_download"]+topic_id_list[-1])
-
-	download_url = "http://leo.rp.edu.sg"+ re.search('HREF=\"(.+?zip)',get_download_url.read()).groups()[0]
-	print download_url
-	response.set("Downloading")
-	os.chdir(os.path.expanduser("~/Desktop"))
-	zip_file = open("problem.zip","wb")
-	zip_file.write(	leo.open_url(download_url).read() )
-	zip_file.close()
-	extractDirectory=""
-	file=open("problem.zip")
-
-##WillYan
-	zfile=zipfile.ZipFile(file)
-	zip_dirs=zfile.namelist()
-	zfile.extractall()
-	new_folder=os.getcwd()+"/"+"Problem"+str(len(project_id_list))+"/"
-	os.makedirs(new_folder)
-	count=0
-	for i in zip_dirs:
-	   extractDirectory=os.getcwd()+"/"+str(i)
-	   filename=zip_dirs[count][(zip_dirs[count].rfind("/")+1)::]
-	   if(os.path.isdir(extractDirectory)==False):
-	      shutil.copyfile(extractDirectory,(new_folder+"/"+filename))
-	   count+=1
-	os.remove("problem.zip")
-	shutil.rmtree("Databank-CurrentSemester")
-	response.set("Done!")
+	
+	try:
+		project_id_list = leo.parse_id("projectid",leo.open_url(URL_LIST["current_module"]))
+		group_id_list = leo.parse_id("groupid",leo.open_url(URL_LIST["current_module"]))
+		topic_id_list = leo.parse_id("topicid",leo.open_url(URL_LIST["current_problem"]+
+											"projectid="+str(project_id_list[-1])+
+											"&groupid="+str(group_id_list[-1])))
+		try:
+			get_download_url = leo.open_url(URL_LIST["problem_download"]+topic_id_list[-1])
+			download_url = "http://leo.rp.edu.sg"+ re.search('HREF=\"(.+?zip)',get_download_url.read()).groups()[0]
+			print download_url
+			response.set("Downloading")
+			os.chdir(os.path.expanduser("~/Desktop"))
+			zip_file = open("problem.zip","wb")
+			zip_file.write(	leo.open_url(download_url).read() )
+			zip_file.close()
+			extractDirectory=""
+			newfile=open("problem.zip")
+		##WillYan
+			zfile=zipfile.ZipFile(newfile)
+			zip_dirs=zfile.namelist()
+			zfile.extractall()
+			new_folder=os.getcwd()+"/"+"Problem"+str(len(project_id_list))+"/"
+			os.makedirs(new_folder)
+			count=0
+			for i in zip_dirs:
+			   extractDirectory=os.getcwd()+"/"+str(i)
+			   filename=zip_dirs[count][(zip_dirs[count].rfind("/")+1)::]
+			   if(os.path.isdir(extractDirectory)==False):
+			      shutil.copyfile(extractDirectory,(new_folder+"/"+filename))
+			   count+=1
+			os.remove("problem.zip")
+			shutil.rmtree("Databank-CurrentSemester")
+			response.set("Done!")
+		except:
+			tkMessageBox.showwarning("Information","You do not have any problem statements to download for today. Hooray!")
+	except:
+		tkMessageBox.showwarning("Wrong credentials","Error connecting to the LEO server")
 
 def download_thread(dummy=1):	
 	threading.Thread(target=download).start()
